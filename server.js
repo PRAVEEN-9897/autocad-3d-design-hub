b1df5f383fca8de1aacff9f02dc248a032b96317
Praveen Bhardwaj <bhardwajpraveen352@gmail.com>
2026-06-14 20:10:52 +0530

    mail test

--- server.js
+++ server.js
@@ -52,15 +52,10 @@ const upload = multer({ storage: storage });
 
 
 const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
+    service: "gmail",
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
-    },
-    tls: {
-        rejectUnauthorized: false
     }
 });
 
