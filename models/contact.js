0c14bf78d2d113590e4d431116174478ab6fc372
Praveen Bhardwaj <bhardwajpraveen352@gmail.com>
2026-06-15 00:24:30 +0530

    some changes

--- models/contact.js
+++ models/contact.js
@@ -24,24 +24,3 @@ const contactSchema = new mongoose.Schema({
 });
 
 module.exports = mongoose.model("Contact", contactSchema);
-const contactForm = document.getElementById("contactForm");
-
-contactForm.addEventListener("submit", async function(e){
-
-    e.preventDefault();
-
-    console.log("FORM SUBMITTED");
-
-    const formData = new FormData(contactForm);
-
-    const data = {
-        name: formData.get("name"),
-        email: formData.get("email"),
-        phone: formData.get("phone"),
-        message: formData.get("message")
-    };
-
-    console.log(data);
-
-    alert("Button Clicked");
-});
