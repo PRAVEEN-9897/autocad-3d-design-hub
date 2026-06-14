dd08ec763a833637abd7f93f0e59dc3ce9bb3e13
Praveen Bhardwaj <bhardwajpraveen352@gmail.com>
2026-06-14 19:44:50 +0530

    mail test

--- server.js
+++ server.js
@@ -72,6 +72,22 @@ transporter.verify(function (error, success) {
     }
 });
 
+app.get("/mailtest", async (req, res) => {
+    try {
+        await transporter.sendMail({
+            from: process.env.EMAIL_USER,
+            to: process.env.EMAIL_USER,
+            subject: "Test Mail",
+            text: "Hello"
+        });
+
+        res.send("Mail Sent");
+    } catch (err) {
+        console.log(err);
+        res.send(err.message);
+    }
+});
+
 
 /* MIDDLEWARE */
 
