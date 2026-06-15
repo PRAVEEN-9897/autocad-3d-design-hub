ee8dd7ca87bfa7bfe508f35a7c3c02e02a00e551
Praveen Bhardwaj <bhardwajpraveen352@gmail.com>
2026-06-15 08:30:49 +0530

    changes

--- public/contact.js
+++ public/contact.js
@@ -1,8 +1,29 @@
-// console.log("CONTACT JS LOADED");
+const contactForm = document.getElementById("contactForm");
 
-// const contactForm = document.getElementById("contactForm");
+contactForm.addEventListener("submit", async function(e){
 
-// contactForm.addEventListener("submit", function(e){
-//     e.preventDefault();
-//     alert("FORM SUBMITTED");
-// });
+    e.preventDefault();
+
+    const formData = new FormData(contactForm);
+
+    const data = {
+        name: formData.get("name"),
+        email: formData.get("email"),
+        phone: formData.get("phone"),
+        message: formData.get("message")
+    };
+
+    console.log(data);
+
+    const response = await fetch("/contact", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+    });
+
+    const result = await response.text();
+
+    alert(result);
+});
